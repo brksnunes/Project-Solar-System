1bc92495d8e75f13d1dee9c43c96411ee9f13483
Rodrigo Nunes <brksnunes@protonmail.com>
2022-07-20 18:41:29 -0300

    feat: :sparkles: Rendered 'MissionCard' inside 'Missions'
    Rendered 'MissionCard' inside 'Missions' with the data provided, according with requirement 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    const missionCardHtmlElement = missions.map((mission) => (<MissionCard
+      key={ mission.name }
+      name={ mission.name }
+      year={ mission.year }
+      country={ mission.country }
+      destination={ mission.destination }
+    />));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missionCardHtmlElement }
       </div>
     );
   }
