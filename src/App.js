b8ef5dc00256eb5b7663bce27461daa129f1e2c9
Rodrigo Nunes <brksnunes@protonmail.com>
2022-07-20 15:14:49 -0300

    feat: :sparkles: Added Header component
    Implemented 'Header' component, that contains the page's main title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 // oliva <3
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
 
